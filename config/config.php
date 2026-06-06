cf69b628d755dec3d3094402e280cac4c9437c2d
Dulan Nithila Liyanarachchi <nithila0411@gmail.com>
2026-06-06 14:08:05 +0530

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,12 @@
 <?php
-    $dbuser="root";
-    $dbpass="";
-    $host="localhost";
-    $db="lms";
+    require '../vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
+    $host   = $_ENV['DB_HOST'];
+    $db     = $_ENV['DB_NAME'];
+    $dbuser = $_ENV['DB_USER'];
+    $dbpass = $_ENV['DB_PASS'];
     $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
 ?>
