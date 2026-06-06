59f2802663576056ab160f8e894515be34c40117
Dulan Nithila Liyanarachchi <nithila0411@gmail.com>
2026-06-06 14:09:47 +0530

    Update config.php

--- payment/config.php
+++ payment/config.php
@@ -1,22 +1,27 @@
 <?php
+    require '../vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+
     return [
         'merchant_id' => '1235867',
         'merchant_secret' => 'MTY3MzQxNjc5ODMxMTQ3Mzc2NTMxNTk2OTc5MzM1MjQ0NDQ2NjgwMw==',
 
         'db' => [
-            'host' => '127.0.0.1',
-            'user' => 'root',
-            'pass' => '',
-            'name' => 'lms',
+            'host' => $_ENV['DB_HOST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'name' => $_ENV['DB_NAME'],
         ],
 
         'smtp' => [
-            'host' => 'smtp.gmail.com',
-            'username' => 'instructlylms@gmail.com',
-            'password' => 'pkrryqxojpodycts',
-            'port' => 465,
-            'from_email' => 'instructlylms@gmail.com',
-            'from_name' => 'Instructly LMS',
+            'host' => $_ENV['SMTP_HOST'],
+            'username' => $_ENV['SMTP_USERNAME'],
+            'password' => $_ENV['SMTP_PASSWORD'],
+            'port' => $_ENV['SMTP_PORT'],
+            'from_email' => $_ENV['FROM_EMAIL'],
+            'from_name' => $_ENV['FROM_NAME'],
         ],
     ];
 ?>
