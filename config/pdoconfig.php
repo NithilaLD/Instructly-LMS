521146be9e4618ef5dcf1cc824fccf002a236a83
Dulan Nithila Liyanarachchi <nithila0411@gmail.com>
2026-06-06 14:08:28 +0530

    Update pdoconfig.php

--- config/pdoconfig.php
+++ config/pdoconfig.php
@@ -1,14 +1,21 @@
 <?php
-$DB_host = "localhost";
-$DB_user = "root";
-$DB_pass = "";
-$DB_name = "lms";
-try
-{
- $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
- $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
- $e->getMessage();
-}
+    require '../vendor/autoload.php';
+
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+    $dotenv->load();
+    
+    $DB_host = $_ENV['DB_HOST'];
+    $DB_user = $_ENV['DB_USER'];
+    $DB_pass = $_ENV['DB_PASS'];
+    $DB_name = $_ENV['DB_NAME'];
+
+    try
+    {
+        $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+        $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        $e->getMessage();
+    }
+?>
